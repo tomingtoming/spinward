90597e8028e4057a3907d93edddb40e7549d03a2
toming <tomingtoming@gmail.com>
2026-06-11 21:24:36 +0900

    Add CI and GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import basicSsl from '@vitejs/plugin-basic-ssl'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: process.env.VITE_BASE_PATH ?? '/',
   plugins: [basicSsl()],
   server: {
     host: '0.0.0.0',
